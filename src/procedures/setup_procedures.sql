2efb9305e8f769c941223e798c10cc6cbcef76f0
vyzyv <vyz@protonmail.com>
2017-06-23 18:36:08 +0200

    Fix adding mind maps to DB

--- src/procedures/setup_procedures.sql
+++ src/procedures/setup_procedures.sql
@@ -6,7 +6,8 @@ DROP PROCEDURE IF EXISTS add_node;
 
 CREATE DEFINER = CURRENT_USER PROCEDURE add_node (
 		IN parent_id BIGINT,
-		IN text_content TEXT
+		IN text_content TEXT,
+    OUT inserted_id BIGINT
 )
 
 BEGIN
@@ -15,10 +16,11 @@ BEGIN
 
 	START TRANSACTION;
       UPDATE tree SET rgt = rgt + 2 WHERE rgt >= @new_right;
-      UPDATE tree SET lft = lft + 2 WHERE lft >= @new_right;
+      UPDATE tree SET lft = lft + 2 WHERE lft > @new_right;
 
 			INSERT INTO tree (content, lft, rgt)
 					 VALUES (text_content, @new_right , @new_right + 1);
+      SELECT LAST_INSERT_ID() INTO inserted_id;
 
 	COMMIT;
 
@@ -40,8 +42,8 @@ proc: BEGIN
 	FROM tree AS node,
 			 tree AS parent
 	WHERE node.lft BETWEEN parent.lft AND parent.rgt
-				AND node.content = searched
-				/* (SELECT content FROM tree WHERE match(content) AGAINST(searched IN BOOLEAN MODE) LIMIT 1 OFFSET off) */
+				AND node.content =
+				(SELECT content FROM tree WHERE match(content) AGAINST(searched IN BOOLEAN MODE) LIMIT 1 OFFSET off)
 	ORDER BY parent.lft;
 
 END //
